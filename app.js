5463fa70256d3935d7bb324ab93e772cb542867b
site-demo <site9demo@monumentmail.com>
2015-07-31 01:07:51 -0500

    adding static posts

--- app.js
+++ app.js
@@ -8,4 +8,12 @@ app.controller('MainCtrl', [
 	'$scope',
 	function ($scope) {
 		$scope.test = "Hello from Buzz Feeder!";
+
+		$scope.posts = [
+			{ title: 'Post 1', upvotes: 5 },
+			{ title: 'Post 2', upvotes: 2 },
+			{ title: 'Post 3', upvotes: 15 },
+			{ title: 'Post 4', upvotes: 9 },
+			{ title: 'Post 5', upvotes: 4 }
+		];
 	}]);
