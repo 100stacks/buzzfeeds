00ebbe5525a024baf82f80c0b9dde618aea8ade4
site-demo <site9demo@monumentmail.com>
2015-08-02 02:11:39 -0500

    Adding Posts Service (as a factory)

--- app.js
+++ app.js
@@ -4,18 +4,35 @@
 
 var app = angular.module('buzzFeeder', []);
 
+app.factory('posts', [function() {
+	// posts - service body
+	// 		- refactoring our $scope.posts variable into service
+	//     	- we are using a Factory instead of Service or Provider (http://tylermcginnis.com/angularjs-factory-vs-service-vs-provider/)
+
+	var obj = {
+		posts: []
+	};
+
+	return obj;
+
+}]);
+
 app.controller('MainCtrl', [
 	'$scope',
-	function ($scope) {
+	'posts',							// inject the 'posts' service into our controller so we can access its data
+	function ($scope, posts) {
 		$scope.test = "Hello from Buzz Feeder!";
 
-		$scope.posts = [
-			{ title: 'Post 1', upvotes: 5 },
-			{ title: 'Post 2', upvotes: 2 },
-			{ title: 'Post 3', upvotes: 15 },
-			{ title: 'Post 4', upvotes: 9 },
-			{ title: 'Post 5', upvotes: 4 }
-		];
+		$scope.posts = posts.posts;		// now any change or modification made to $scope.posts will be stored in the service,
+										// and immediately accessible by any other module that injects the posts service. 
+
+		// $scope.posts = [
+		// 	{ title: 'Post 1', upvotes: 5 },
+		// 	{ title: 'Post 2', upvotes: 2 },
+		// 	{ title: 'Post 3', upvotes: 15 },
+		// 	{ title: 'Post 4', upvotes: 9 },
+		// 	{ title: 'Post 5', upvotes: 4 }
+		// ];
 
 		$scope.addPost = function() {
 
@@ -26,7 +43,7 @@ app.controller('MainCtrl', [
 			$scope.posts.push({ title: $scope.title,
 								link: $scope.link, 
 								upvotes:0 });
-			
+
 			$scope.title = ''; 
 			$scope.link = '';
 		};
